1a8917551be64123d2046263345d7a8b83f78063
Alexander Surmann <a.surmann@hotmail.com>
2022-11-07 22:03:49 +0100

    Yelp Camp Controllers
    Section 53: YelpCamp: Controllers & Star Ratings

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const Campground = require('../models/campground.js');
-const Review = require('../models/review.js');
+const reviews = require('../controllers/reviews.js');
 const catchAsync = require('../utils/catchAsync.js');
 const {
   validateReview,
@@ -9,33 +8,13 @@ const {
   isReviewAuthor,
 } = require('../middleware.js');
 
-router.post(
-  '/',
-  isLoggedIn,
-  validateReview,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created new Review');
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 router.delete(
   '/:reviewId',
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully deleted review.');
-    res.redirect(`/campgrounds/${id}`);
-  })
+  catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
